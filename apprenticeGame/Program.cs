ee82b8b0ec9fc9af5979224428ef5891d9f72166
jheaden <jheaden@gmail.com>
2019-09-02 12:58:53 -0700

    please work

---
+++ apprenticeGame/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PotionGame
+{
+    class Program
+    {
+
+        //TODO Add more characters and things to do
+        static void Main(string[] args)
+        {
+            Console.WriteLine("While adventuring in the forest, you find a witch's hut. The witch does not appear to be home, so you go inside.");
+            Console.WriteLine("\"Hello,\" says a black cat. \"Would you like to play a game?\"");
+            Console.WriteLine("You resist the urge to say \"I thought I was already playing one...\" and instead you say: \"Sure!\"");
+            Console.WriteLine("The cat hops over to a table with three potions on it and says \"Choose one.\"");
+
+            Red_Potion drinky = new Red_Potion();
+            drinky.Affect();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ apprenticeGame/Red_Potion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PotionGame
+{
+    class Red_Potion : IPotion
+    {
+        public void Affect()
+        {
+            Console.WriteLine("Without hesitating, you drink the red potion. There is a flash of light and you feel a little queasy.");
+            Console.WriteLine("Then you notice that you have turned into a cat.");
+        }
+
+     
+    }
+}
